40a6da52469a813281ace33fc63c327b9d52eeef
Vladimirs <vladimirribakov123@mail.ru>
2022-01-24 21:48:32 +0000

    Finished Test Validation

--- Week17-ErrorHandlingWebsite/ErrorProneWebsite.Tests/FileManagerTest.cs
+++ Week17-ErrorHandlingWebsite/ErrorProneWebsite.Tests/FileManagerTest.cs
@@ -9,7 +9,7 @@ namespace ErrorProneWebsite.Tests
     [TestClass]
     public class FileManagerTest
     {
-        private const string TEST_FILE_PATH = @"C:\Users\Sulur\Documents\Desktop\New folder\Week17-ErrorHandlingWebsite\ErrorProneWebsite.Tests\TestContent\TestContent.txt";
+        private const string TEST_FILE_PATH = @"C:\Users\vladi\Desktop\Advanced Programming\CTEC2902-Labs-1\Week17-ErrorHandlingWebsite\ErrorProneWebsite.Tests\TestContent\TestContent.txt";
 
         [TestMethod]
         public void TheFileManagerCanReadAFile()
@@ -21,18 +21,21 @@ namespace ErrorProneWebsite.Tests
 
         }
 
-
         [TestMethod]
-        [ExpectedException(typeof(FileNotFoundException))]
         public void TheFileManagerHandlesAMissingFile()
         {
             FileManager fileManager =
-            new FileManager(@"D:\MissingFileThereIsNoFileHere.txt");
-
-            Assert.IsTrue(fileManager.GetEvenMoreContent().Contains("The content file doesn't exist in the location specified!!!!!..."));
-
+            new FileManager(@"C:\Users\vladi\Desktop\Advanced Programming\CTEC2902-Labs-1\Week17-ErrorHandlingWebsite\ErrorProneWebsite.Tests\TestContent\TestContent1.txt");
+            Assert.IsTrue(fileManager.GetContent().Contains("Oops! The content could not be found at the location specified."));
         }
 
-
+        [TestMethod]
+        [ExpectedException(typeof(FileNotFoundException))]
+        public void TheSystemThrowsAFileNotFoundExceptionWhenGivenAPathToAMissingFile()
+        {
+            FileManager fileManager = new
+            FileManager(@"C:\MissingFileThereIsNoFileHere.txt");
+            Assert.IsTrue(fileManager.GetEvenMoreContent().Contains("This line won't execute as the exception will be thrown before it's hit"));
+        }
     }
 }
