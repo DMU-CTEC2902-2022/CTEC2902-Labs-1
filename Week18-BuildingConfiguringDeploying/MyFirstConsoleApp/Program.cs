da680ef761fa83c325af3583fe01fe344ce6ba40
DMU-CTEC2902-2021 <umanna00@dmu.ac.uk>
2022-02-17 09:44:04 +0000

    Usama Updated

---
+++ Week18-BuildingConfiguringDeploying/MyFirstConsoleApp/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using Console = Colorful.Console;
+using System.Configuration;
+
+namespace MyFirstConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Debug.WriteLine("Write something to the output window...");
+            Console.WriteLine("Hello world!");
+            Console.WriteLine("Program has finished. Press enter to quit.", Color.Aquamarine);
+
+            string greeting = Properties.Settings.Default.GreetingMessage;
+            Console.WriteLine(greeting);
+
+            if(Properties.Settings.Default.InAGoodMood)
+                Console.WriteLine(Properties.Settings.Default.AngryMessage);
+
+            string tiredMessage = ConfigurationManager.AppSettings["tiredMessage"];
+            Console.WriteLine(tiredMessage);
+
+
+            Console.ReadKey();            
+        }
+    }
+}
